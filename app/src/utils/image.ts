0f9fd8d5b45edfb9a34606e6c661c86fe8b644ef
Dalton Craven <7117993+cravend@users.noreply.github.com>
2026-02-26 16:29:46 +0100

    chore: update and configure everything

--- app/src/utils/image.ts
+++ app/src/utils/image.ts
@@ -1,8 +1,8 @@
-import imageUrlBuilder from "@sanity/image-url";
+import { createImageUrlBuilder } from "@sanity/image-url";
 import type { Image } from "@sanity/types";
 import { sanityClient } from "sanity:client";
 
-const builder = imageUrlBuilder(sanityClient);
+const builder = createImageUrlBuilder(sanityClient);
 
 export function urlFor(source: Image) {
   return builder.image(source);
--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,7 @@
  * Learn more: https://www.sanity.io/docs/cli
  */
 
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
@@ -16,5 +16,8 @@ export default defineCliConfig({
     dataset,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
-  autoUpdates: true,
+  deployment: {
+    autoUpdates: true,
+    appId: 'r33n636vnilnc6ri8ei1qn3g',
+  },
 })
